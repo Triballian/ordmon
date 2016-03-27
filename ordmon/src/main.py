1882062dfe1b2bed607d2fd637530e93e1c5c464
Triballian <candiapps@gmail.com>
2016-03-27 14:41:03 -0500

    http://stackoverflow.com/questions/10807081/script-to-extract-data-from-web-page solution

--- ordmon/src/main.py
+++ ordmon/src/main.py
@@ -3,6 +3,16 @@ Created on Mar 27, 2016
 
 @author: Noe
 '''
+import requests
+import bs4
+
+session = requests.session()
+
+req = session.get('http://stackoverflow.com/questions/10807081/script-to-extract-data-from-web-page')
+
+doc = bs4.BeautifulSoup(req.content)
+
+print doc.findAll('a', { "class" : "gp-share" })
 
 if __name__ == '__main__':
     pass
