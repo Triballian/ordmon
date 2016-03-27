5d361c6d1c7a52e0a50ad8da274549e99e2b91c2
Triballian <candiapps@gmail.com>
2016-03-27 15:06:01 -0500

    working version

--- ordmon/src/main.py
+++ ordmon/src/main.py
@@ -10,9 +10,9 @@ session = requests.session()
 
 req = session.get('http://stackoverflow.com/questions/10807081/script-to-extract-data-from-web-page')
 
-doc = bs4.BeautifulSoup(req.content)
+doc = bs4.BeautifulSoup(req.content,'lxml')
 
-print doc.findAll('a', { "class" : "gp-share" })
+print doc.findAll('a', { "class" : "js-gps-track" })
 
 if __name__ == '__main__':
     pass
