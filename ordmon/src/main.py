dd0491c360fc2a9075881dedf46db331914634b0
Triballian <candiapps@gmail.com>
2016-03-27 17:47:55 -0500

    trying to integrate cookies

--- ordmon/src/main.py
+++ ordmon/src/main.py
@@ -8,11 +8,13 @@ import bs4
 
 session = requests.session()
 
-req = session.get('http://stackoverflow.com/questions/10807081/script-to-extract-data-from-web-page')
+req = session.get('https://c-cex.com/?id=orders')
 
 doc = bs4.BeautifulSoup(req.content,'lxml')
+#firs param is tag name
 
-print doc.findAll('a', { "class" : "js-gps-track" })
+print doc
+# print doc.findAll('td', { "class" : "hand" })
 
 if __name__ == '__main__':
     pass
