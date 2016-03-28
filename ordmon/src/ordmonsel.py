370fd0437c6fc8dec1fa8e883370abe722535289
Triballian <candiapps@gmail.com>
2016-03-28 16:30:46 -0500

    Cookie loading is working

--- ordmon/src/ordmonsel.py
+++ ordmon/src/ordmonsel.py
@@ -7,20 +7,39 @@ just place chromedriver.exe into the same directory as this app
 
 @author: Noe
 '''
+import pickle
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import selenium2libary
-
 import time
+import os
 
-path_to_chromedriver = 'chromedriver.exe'
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--test-type')
+class ordmonsel:
+
+    
 
 # browser = webdriver.Chrome(executable_path=path_to_chromedriver)
 
 # browser = webdriver.Chrome(executable_path=path_to_chromedriver)
-browser = webdriver.Chrome(executable_path=path_to_chromedriver, chrome_options=chrome_options)
+    def setupbrowser(self):
+        path_to_chromedriver = 'chromedriver.exe'
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--test-type')
+        return webdriver.Chrome(executable_path=path_to_chromedriver, chrome_options=chrome_options)
+    
+    
+    def loadcookies(self):
+        if os.path.isfile('cookies.pkl'):
+            cookies = pickle.load(open('cookies.pkl', 'rb'))
+            for cookie in cookies:
+                browser.add_cookie(cookie)
+                
+    def savecookies(self):
+        pickle.dump( browser.get_cookies() , open("cookies.pkl","wb"))
+        
+        
+        
+    
 # broswer.Chrome(chrome_options=chrome_options)
 
 # browser.create_options('test-type')
@@ -30,17 +49,32 @@ url = 'https://c-cex.com/?id=orders'
 
 
 if __name__ == '__main__':
-    browser.get(url)
+    
+#     ordmons = ordmonsel
+#     browserconf = ordmonsel
+    browserconf = ordmonsel()
+   
+    browser = browserconf.setupbrowser()
+    
+    
+#    browser.get('https://www.google.com')
 #     open tab
-    time.sleep(30)
-    browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-#    browser.findElement(By.linkText('urlLink')).sendKeys()
+#    time.sleep(30)
+#    browserconf.loadcookies()
+#    browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+    browser.get(url)
+    time.sleep(10)
+    browserconf.loadcookies()
+    browser.get(url)
+#     browser.find_element(browser.by.LINK_TEXT(url)).sendKeys()
+#    browser.
     print 'loding page'
     time.sleep(5)
     print 'Login to C-Cex'
     time.sleep(60)
-    browser.find_element_by_tag_name('h1')
-    
+    browserconf.savecookies()
+    print 'saving cookies'
+#    browser.find_element_by_tag_name('h1')    
     
      
     pass
