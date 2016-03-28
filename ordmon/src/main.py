d22b7e1de349f7e9a801be4c94b33132e779ec01
Triballian <candiapps@gmail.com>
2016-03-27 20:01:38 -0500

    working main

--- ordmon/src/main.py
+++ ordmon/src/main.py
@@ -4,21 +4,18 @@ Created on Mar 27, 2016
 @author: Noe
 '''
 import requests
-import selenium
-
-
 import bs4
 
-session = requests.session()
 
-req = session.get('https://c-cex.com/?id=orders')
-cookies = requests.utils.dict_from_cookiejar
-driver = requests.webdriver
-doc = bs4.BeautifulSoup(req.content,'lxml')
-#firs param is tag name
-
-print doc
-# print doc.findAll('td', { "class" : "hand" })
 
 if __name__ == '__main__':
+    session = requests.session()
+
+    req = session.get('https://c-cex.com/?id=orders')
+    
+    doc = bs4.BeautifulSoup(req.content,'lxml')
+    #firs param is tag name
+    
+    print doc
+# print doc.findAll('td', { "class" : "hand" })
     pass
