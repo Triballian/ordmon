7ae94c1a12faf873cf2575fda666fba32337a109
Triballian <candiapps@gmail.com>
2016-03-27 19:46:29 -0500

    downloaded ms cookie finder

--- ordmon/src/main.py
+++ ordmon/src/main.py
@@ -4,12 +4,16 @@ Created on Mar 27, 2016
 @author: Noe
 '''
 import requests
+import selenium
+
+
 import bs4
 
 session = requests.session()
 
 req = session.get('https://c-cex.com/?id=orders')
-
+cookies = requests.utils.dict_from_cookiejar
+driver = requests.webdriver
 doc = bs4.BeautifulSoup(req.content,'lxml')
 #firs param is tag name
 
---
+++ ordmon/src/ordmonsel.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 27, 2016
+need to download chrome driver http://chromedriver.storage.googleapis.com/index.html
+I use 2.9 win32
+just place chromedriver.exe into the same directory as this app
+
+@author: Noe
+'''
+
+
+if __name__ == '__main__':
+    pass
