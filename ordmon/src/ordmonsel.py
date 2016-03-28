b48ce5d19b9bcb7d01ee5dd1df5b5985da90faa3
Triballian <candiapps@gmail.com>
2016-03-28 04:47:59 -0500

    added open new tab

--- ordmon/src/ordmonsel.py
+++ ordmon/src/ordmonsel.py
@@ -5,10 +5,13 @@ need to download the program chrome driver http://chromedriver.storage.googleapi
 I use 2.9 win32
 just place chromedriver.exe into the same directory as this app
 
-
 @author: Noe
 '''
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# import selenium2libary
+
+import time
 
 path_to_chromedriver = 'chromedriver.exe'
 chrome_options = webdriver.ChromeOptions()
@@ -28,5 +31,16 @@ url = 'https://c-cex.com/?id=orders'
 
 if __name__ == '__main__':
     browser.get(url)
+#     open tab
+    time.sleep(30)
+    browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+#    browser.findElement(By.linkText('urlLink')).sendKeys()
+    print 'loding page'
+    time.sleep(5)
+    print 'Login to C-Cex'
+    time.sleep(60)
+    browser.find_element_by_tag_name('h1')
     
+    
+     
     pass
