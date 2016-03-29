919cb8238e80a01ff0bf9898989cd9f3bbb82330
Triballian <candiapps@gmail.com>
2016-03-29 15:26:12 -0500

    added better instructions

--- ordmon/src/ordmonsel.py
+++ ordmon/src/ordmonsel.py
@@ -4,6 +4,12 @@ certain sites do not like being scraped, to get arround this python needs to pre
 need to download the program chrome driver http://chromedriver.storage.googleapis.com/index.html
 I use 2.9 win32
 just place chromedriver.exe into the same directory as this app
+Instructions:
+This has limited features and so requires some playing with at least until someone else or I make it more user friendly
+You need to be logged in for it to work. Aster the first time you log in it should be able to run automatically,
+you can open up a new tab and use the c-cex website normally but you don't want to work on the first tab the app opens
+any questions or comments you can contact me at 
+noe@stakeco.in 
 
 @author: Noe
 '''
